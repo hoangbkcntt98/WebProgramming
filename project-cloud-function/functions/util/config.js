b13f605c1cfc13a96b2990f77f5d07d92e643fe0
Tung Nguyen <ngtientung98@gmail.com>
2020-11-13 20:35:22 +0100

    add api and function cloud with firebase

---
+++ project-cloud-function/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDzqPVoJJc54XETNqJ3pcmYlEfXmuuRmgs",
+    authDomain: "project-management-4a011.firebaseapp.com",
+    databaseURL: "https://project-management-4a011.firebaseio.com",
+    projectId: "project-management-4a011",
+    storageBucket: "project-management-4a011.appspot.com",
+    messagingSenderId: "457416106641",
+    appId: "1:457416106641:web:4d954247746c40ffe8d530",
+    measurementId: "G-LNWYVYG1T6"
+};
