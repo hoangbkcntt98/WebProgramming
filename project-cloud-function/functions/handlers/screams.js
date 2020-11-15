5a3943bd5510519c7a91fcbaa17e808abe28c36d
Hoang Do <20166135@student.hust.edu.vn>
2020-11-15 11:51:05 +0100

    fix bug in getScream cloud-function and create ScreamDialog

--- project-cloud-function/functions/handlers/screams.js
+++ project-cloud-function/functions/handlers/screams.js
@@ -64,7 +64,7 @@ exports.getScream = (req, res) => {
       screamData.screamId = doc.id;
       return db
         .collection('comments')
-        .orderBy('createdAt', 'desc')
+        // .orderBy('createdAt', 'desc')
         .where('screamId', '==', req.params.screamId)
         .get();
     })
--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -1,5 +1,6 @@
 import {
     SET_SCREAMS,
+    SET_SCREAM,
     LOADING_DATA,
     LIKE_SCREAM,
     UNLIKE_SCREAM,
@@ -7,7 +8,8 @@ import {
     POST_SCREAM,
     SET_ERRORS,
     CLEAR_ERRORS,
-    LOADING_UI
+    LOADING_UI,
+    STOP_LOADING_UI
   } from '../types';
   import axios from 'axios';
   
@@ -82,4 +84,20 @@ export const postScream = (newScream) => (dispatch) => {
         payload: err.response.data
       });
     });
+  };
+  //get a scream
+  export const getScream = (screamId) => (dispatch) => {
+    dispatch({type: LOADING_UI});
+    axios.get(`https://europe-west1-project-management-4a011.cloudfunctions.net/api/scream/${screamId}`)
+    .then((res) => {
+      dispatch({
+        type: SET_SCREAM,
+        payload:res.data
+      });
+      dispatch({type:STOP_LOADING_UI});
+    })
+    .catch(err => console.log(err));
+  }
+  export const clearErrors = () => (dispatch) => {
+    dispatch({ type: CLEAR_ERRORS });
   };
